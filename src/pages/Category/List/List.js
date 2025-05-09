0ff7480b2e06500f7b55426e4705f0df494b9c04
huyvomieu <huy040424@gmail.com>
2025-05-09 17:06:09 +0700

    fix: sửa ui danh mục

--- src/pages/Category/List/List.js
+++ src/pages/Category/List/List.js
@@ -1,20 +1,117 @@
-import CardList from '../../../components/Card/CardList/CardList';
 import { getDeleteCategory } from '../../../services/CategoryService';
-
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+import Button from '../../../components/ui/Button';
+import { useLoading } from '../../../components/context/LoadingContext';
+import { useNavigate } from 'react-router-dom';
+import {
+    Table,
+    TableHeader,
+    TableBody,
+    TableFooter,
+    TableHead,
+    TableRow,
+    TableCell,
+} from '../../../components/ui/Table/Table';
+import { CardTitle } from '../../../components/ui/Card/Card';
+import { Select, SelectValue, SelectTrigger, SelectContent, SelectItem } from '../../../components/ui/Filter/Select';
 function List() {
-    const fetchAPI = async () => {
-        const data = await getDeleteCategory();
-        return data;
-    };
+    const [categories, setCategories] = useState([]);
+    const { setLoading } = useLoading();
+    const navigate = useNavigate();
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setLoading(true);
+            try {
+                const res = await getDeleteCategory();
+                setCategories(res);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchAPI();
+    }, []);
     return (
-        <CardList
-            title="Danh mục"
-            breadcrumbs="Sản phẩm"
-            toBack="/items"
-            toCreate="/category/create"
-            callAPI={fetchAPI}
-            page="category"
-        />
+        <div className="mt-8 mr-8">
+            <div className="">
+                <div className="flex justify-end mb-4">
+                    <Button to="/category/create" primary>
+                        Tạo mới danh mục
+                    </Button>
+                </div>
+                <div className="bg-white shadow-sm">
+                    <div className="p-8">
+                        <div>
+                            <CardTitle>Tất cả danh mục</CardTitle>
+                        </div>
+                        <div className="p-6 flex items-center gap-4 mb-4">
+                            <Select className="" defaultValue="all">
+                                <SelectTrigger className="w-[180px] h-16 border border-solid border-[#e5e5e5]">
+                                    <SelectValue placeholder="Lọc theo trạng thái" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="all">Tất cả danh mục</SelectItem>
+                                    <SelectItem value="show">Danh mục hiển thị</SelectItem>
+                                    <SelectItem value="show">Danh mục ẩn</SelectItem>
+                                </SelectContent>
+                            </Select>
+                            <Select defaultValue="newest">
+                                <SelectTrigger className="w-[180px] h-16 border border-solid border-[#e5e5e5]">
+                                    <SelectValue placeholder="Sắp xếp theo" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="newest">Danh mục mới nhất</SelectItem>
+                                    <SelectItem value="oldest">Danh mục cũ nhất</SelectItem>
+                                    <SelectItem value="highest">Giá lớn nhất</SelectItem>
+                                    <SelectItem value="lowest">Giá nhỏ nhất</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div>
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead>
+                                            <input type="checkbox" />
+                                        </TableHead>
+                                        <TableHead>Ảnh</TableHead>
+                                        <TableHead>Tên Danh mục</TableHead>
+                                        <TableHead>Mô tả</TableHead>
+                                        <TableHead>Trạng thái</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {categories.map((data) => {
+                                        return (
+                                            <TableRow onClick={() => navigate(`/category/${data.key}`)} key={data.key}>
+                                                <TableCell>
+                                                    <input type="checkbox" />
+                                                </TableCell>
+                                                <TableCell>
+                                                    <img
+                                                        className="object-cover w-16 h-16"
+                                                        src={data.ImagePath}
+                                                        alt="Hà nội"
+                                                    />
+                                                </TableCell>
+                                                <TableCell>{data.Name}</TableCell>
+                                                <TableCell>{data.Description}</TableCell>
+                                                <TableCell>
+                                                    <span className="p-2 rounded-full bg-green-400">Hiển thị</span>
+                                                </TableCell>
+                                            </TableRow>
+                                        );
+                                    })}
+                                </TableBody>
+                                <TableFooter></TableFooter>
+                            </Table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/pages/Item/ListItem/ListItem.js
+++ src/pages/Item/ListItem/ListItem.js
@@ -1,8 +1,5 @@
 import classNames from 'classnames';
-import styles from '../Item.module.scss';
 import { getDeleteItem } from '../../../services/ItemService';
-import { RiArrowDownSFill } from 'react-icons/ri';
-import { IoSearch } from 'react-icons/io5';
 import { useEffect, useState } from 'react';
 import Button from '../../../components/ui/Button';
 import { useLoading } from '../../../components/context/LoadingContext';
@@ -15,19 +12,9 @@ import {
     TableHead,
     TableRow,
     TableCell,
-    TableCaption,
 } from '../../../components/ui/Table/Table';
-import { Card, CardTitle } from '../../../components/ui/Card/Card';
-import {
-    Select,
-    SelectGroup,
-    SelectValue,
-    SelectTrigger,
-    SelectContent,
-    SelectItem,
-    SelectScrollUpButton,
-    SelectScrollDownButton,
-} from '../../../components/ui/Filter/Select';
+import { CardTitle } from '../../../components/ui/Card/Card';
+import { Select, SelectValue, SelectTrigger, SelectContent, SelectItem } from '../../../components/ui/Filter/Select';
 
 function Item() {
     const [tours, setTours] = useState([]);
@@ -66,11 +53,9 @@ function Item() {
                                     <SelectValue placeholder="Lọc theo trạng thái" />
                                 </SelectTrigger>
                                 <SelectContent>
-                                    <SelectItem value="all">All Orders</SelectItem>
-                                    <SelectItem value="completed">Completed</SelectItem>
-                                    <SelectItem value="processing">Processing</SelectItem>
-                                    <SelectItem value="pending">Pending</SelectItem>
-                                    <SelectItem value="cancelled">Cancelled</SelectItem>
+                                    <SelectItem value="all">Tất cả Tour</SelectItem>
+                                    <SelectItem value="show">Tour hiển thị</SelectItem>
+                                    <SelectItem value="hide">Tour ẩn</SelectItem>
                                 </SelectContent>
                             </Select>
                             <Select defaultValue="newest">
@@ -78,8 +63,8 @@ function Item() {
                                     <SelectValue placeholder="Sắp xếp theo" />
                                 </SelectTrigger>
                                 <SelectContent>
-                                    <SelectItem value="newest">Đơn hàng mới nhất</SelectItem>
-                                    <SelectItem value="oldest">Đơn hàng cũ nhất</SelectItem>
+                                    <SelectItem value="newest">Tour mới nhất</SelectItem>
+                                    <SelectItem value="oldest">Tour cũ nhất</SelectItem>
                                     <SelectItem value="highest">Giá lớn nhất</SelectItem>
                                     <SelectItem value="lowest">Giá nhỏ nhất</SelectItem>
                                 </SelectContent>
